e4158f31439fd5e07b209ecc00014a0b6af80401
Joshua Hill <posixninja@gmail.com>
2010-05-27 15:18:56 -0400

    Added licence infomation README and TODO

--- includes/commands.h
+++ includes/commands.h
@@ -1,9 +1,21 @@
-/*
- *
- *  greenpois0n - payload/include/commands.h
- *  (c) 2010 Chronic-Dev Team
- *
- */
+/**
+  * GreenPois0n Cynanide - commands.h
+  * Copyright (C) 2010 Chronic-Dev Team
+  * Copyright (C) 2010 Joshua Hill
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 
 #ifndef COMMANDS_H
 #define COMMANDS_H
